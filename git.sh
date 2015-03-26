3dde9d566790880e4bcdb5f450e9ad30069daf93
Hamid R. Darabi, Ph.D <hdarabi@gmail.com>
2015-03-26 13:44:59 -0400

    In dev. v0.0.0

---
+++ git.sh
@@ -0,0 +1,42 @@
+clear
+git_path = which git
+if [ -z $git_path ]
+then
+	echo "git exists"
+else
+	echo "installing git"
+	sudo apt-get update
+	sudo apt-get install git
+fi
+
+sudo adduser git
+su git
+cd
+makdir .ssh && chmod 700 .ssh
+touch .ssh/authorized_keys && chmod .ssh/authorized_keys
+echo "Please enter the path to the git user public key"
+read key_path
+cat $key_path >> ~/.ssh/authorized_keys
+cd /opt/git
+echo "Please enter project name"
+read proj_name
+mkdir $proj_name
+cd $proj_name
+git init --bare
+
+git_shell = which git-shell
+in_shell = grep $git_shell, /etc/shells
+if [ z in_shell ]
+then
+	cat /etc/shells $git_shell	
+fi
+
+echo "Please enter git_only users. Enter to end"
+read user_name
+while [ -z $user_name ]
+do
+	sudo chsh $user_name $git_shell
+	echo "Please enter git_only users. Enter to end"
+	read user_name
+	read user_name
+done
