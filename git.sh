d844b907d143dcd825e2b027caec5bca9cd41d05
Hamid R. Darabi <hdarabi@gmail.com>
2015-03-27 23:48:42 -0400

    In dev v0.0.1

--- git.sh
+++ git.sh
@@ -11,9 +11,10 @@ fi
 
 sudo adduser git
 su git
-cd
-makdir .ssh && chmod 700 .ssh
+cd ~
+mkdir .ssh && chmod 700 .ssh
 touch .ssh/authorized_keys && chmod .ssh/authorized_keys
+ssh-keygen -t rsa -b 4096
 echo "Please enter the path to the git user public key"
 read key_path
 cat $key_path >> ~/.ssh/authorized_keys
@@ -25,18 +26,17 @@ cd $proj_name
 git init --bare
 
 git_shell = which git-shell
-in_shell = grep $git_shell, /etc/shells
-if [ z in_shell ]
+in_shell = grep $git_shell /etc/shells
+if [ ! -z in_shell ]
 then
-	cat /etc/shells $git_shell	
+	cat $git_shell >> /etc/shells
 fi
 
 echo "Please enter git_only users. Enter to end"
 read user_name
-while [ -z $user_name ]
+while [ ! -z $user_name ]
 do
 	sudo chsh $user_name $git_shell
 	echo "Please enter git_only users. Enter to end"
-	read user_name
 	read user_name
 done
